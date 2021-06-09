40ef19f1252fabf8a8f8002a0d88bf65b843c1c5
erriep <tabary.pierre.dev@gmail.com>
2021-06-09 15:49:24 +0200

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const usersRoutes = require('./routes/users');
 
 app.use(express.json());
-
+app.use(cors());
 app.use('/users', usersRoutes);
 
 app.listen(5050, () => {
