0e5962678d5b646e3decdc6b963661fa14110328
AntoineCau <acauwd@gmail.com>
2021-06-16 11:12:39 +0200

    adding get routes for apps

--- routes/apps.js
+++ routes/apps.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 const { db } = require('../conf');
 
-router.get('/apps', async (req, res) => {
+router.get('/', async (req, res) => {
   const sql = 'SELECT * FROM applications';
   const [results] = await db.query(sql);
   res.json(results);
 });
 
-router.get('apps/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const sql = 'SELECT * FROM applications WHERE id=?';
   const sqlValues = [id];
