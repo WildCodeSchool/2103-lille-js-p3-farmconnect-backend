c5dcec6af80beb76d25716bad0d9358cbed90038
CaroD59 <dorchies.c@gmail.com>
2021-06-23 16:25:30 +0200

    ContactForm back

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const cors = require('cors');
 
 const app = express();
 const usersRoutes = require('./routes/users');
+const appsRoutes = require('./routes/apps');
+const contactRoutes = require('./routes/contactform');
 
 app.use(express.json());
 app.use(cors());
 app.use('/users', usersRoutes);
+app.use('/apps', appsRoutes);
+app.use('/contact', contactRoutes);
 
 app.listen(5050, () => {
   console.log('API available on http://localhost:5050 !');
---
+++ routes/contactform.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const router = express.Router();
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+const contactMail = nodemailer.createTransport({
+  service: process.env.SERVICE,
+  auth: {
+    user: process.env.CONTACT_EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+router.post('/', (req, res) => {
+  const { firstName, lastName, email, message, phoneNumber } = req.body;
+  const mailOptions = {
+    From: firstName,
+    to: process.env.CONTACT_MAIL,
+    subject: 'Contact Form Submission',
+    html: `<p>Nom : ${lastName}</p>
+    <p>Prénom : ${firstName}</p>
+    <p>Téléphone : ${phoneNumber}</p>
+    <p>Email : ${email}</p>
+    <p>Message : ${message}</p>
+    `,
+  };
+
+  contactMail.sendMail(mailOptions, (err) => {
+    if (err) {
+      res.json({ status: 'Error sending the message...' });
+    } else {
+      res.json({ status: 'Message sent !' });
+    }
+  });
+});
+
+module.exports = router;
