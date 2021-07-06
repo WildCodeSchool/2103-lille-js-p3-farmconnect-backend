1108d491327230918e0816c912cdb124df35c572
AntoineCau <acauwd@gmail.com>
2021-07-06 22:22:54 +0200

    add applications routes

--- routes/apps.js
+++ routes/apps.js
@@ -4,16 +4,18 @@ const router = express.Router();
 const { db } = require('../conf');
 
 router.get('/', async (req, res) => {
-  const sql = 'SELECT * FROM applications';
+  const sql =
+    'SELECT id, name, description, logo, banner, isFree, app_web, app_android, app_ios, provider_app FROM applications';
   const [results] = await db.query(sql);
   res.json(results);
 });
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const sql = 'SELECT * FROM applications WHERE id=?';
+  const sql =
+    'SELECT id, name, description, logo, banner, isFree, app_web, app_android, app_ios, provider_app FROM applications';
   const sqlValues = [id];
-  const [[results]] = await db.query(sql, sqlValues);
+  const [results] = await db.query(sql, sqlValues);
   res.json(results);
 });
 
--- routes/users.js
+++ routes/users.js
@@ -5,14 +5,15 @@ const { db } = require('../conf');
 
 router.get('/', async (req, res) => {
   const sql =
-    'SELECT id, firstname, lastname, zipcode, city, country, email, mobile FROM users where email=?';
+    'SELECT id, sex, firstname, lastname, zipcode, city, country, email, mobile FROM users';
   const [results] = await db.query(sql);
   res.json(results);
 });
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const sql = 'SELECT email, password FROM users WHERE id=?';
+  const sql =
+    'SELECT id, sex, firstname, lastname, zipcode, city, country, email, mobile FROM users WHERE id=?';
   const sqlValues = [id];
   const [results] = await db.query(sql, sqlValues);
   res.json(results);
@@ -60,9 +61,21 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   const { id } = req.params;
-  const { email, password } = req.body;
-  const sql = 'UPDATE users SET email=?, password=? WHERE id=?';
-  const sqlValues = [email, password, id];
+  const { sex, firstname, lastname, zipcode, city, country, email, mobile } =
+    req.body;
+  const sql =
+    'UPDATE users SET sex=?, firstname=?, lastname=?, zipcode=?, city=?, country=?, email=?, mobile=? WHERE id=?';
+  const sqlValues = [
+    sex,
+    firstname,
+    lastname,
+    zipcode,
+    city,
+    country,
+    email,
+    mobile,
+    id,
+  ];
   try {
     const [results] = await db.query(sql, sqlValues);
     res.status(201).json(results);
