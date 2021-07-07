00f142cd200d76f11803b7edf53076bfc381b2a7
erriep <tabary.pierre.dev@gmail.com>
2021-07-07 16:56:07 +0200

    little change for routes

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const appsRoutes = require('./routes/apps');
 const contactRoutes = require('./routes/contactform');
 const appFavRoutes = require('./routes/appfav');
 
-const appsRoutesAdmin = require('./routes/admin/apps');
-
 app.use(express.json());
 app.use(passport.initialize());
 app.use(cors());
@@ -18,7 +16,6 @@ app.use('/users', usersRoutes);
 app.use('/apps', appsRoutes);
 app.use('/contact', contactRoutes);
 app.use('/appfav', appFavRoutes);
-app.use('/admin/apps', appsRoutesAdmin);
 
 app.use('/auth', require('./routes/auth'));
 
--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const sql = `
   SELECT 
-    sex, firstname, lastname, zipcode, city, country, email, mobile 
+    id, sex, firstname, lastname, zipcode, city, country, email, mobile 
   FROM 
     users 
   WHERE 
